6f57f52da5437379296673b4a87973b602e5bc6e
Jiao Wang <wangjiao_jo@foxmail.com>
2016-12-03 11:06:57 -0500

    Commit

--- lab4/Haoyang_Li_CS546_Lab4/Haoyang_Li_CS546_Lab4/data/index.js
+++
@@ -1,9 +0,0 @@
-const eduData = require("./education");
-const hobData = require("./hobbies");
-const classData = require("./classes");
-
-module.exports = {
-    education: eduData, 
-    hobbies: hobData, 
-    classes: classData
-};
